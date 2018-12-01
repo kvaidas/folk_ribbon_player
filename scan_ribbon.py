9248a2d5ad9b4a56aa26402fea42c5835cbedde1
Marijus Bernotas <marukqs@gmail.com>
2018-12-01 20:13:41 +0200

    Added shit

--- scan_ribbon.py
+++ scan_ribbon.py
@@ -1,18 +1,66 @@
 import cv2
 import numpy
 import time
+import mido
 
 camera = cv2.VideoCapture(0)
-capture_interval = 0.500
+capture_interval = 0.00
 resize_multiplier = .02
 brightness_threshold = 180
+delta_note = 64
+note_multiplier = 4
+controller_delta = 64;
+attrib_coef = (127 - controller_delta) / brightness_threshold
 debug = False
 old_notes = {}
+
+lfo_id=26
+cutoff_id=43
+lfo_coef=2
+lfo_speed_id=24
+
+port = mido.open_output(mido.get_output_names()[2])
+
 
 def process_notes(new_notes):
     global old_notes
     if old_notes.keys() != new_notes.keys():
         print(new_notes)
+      
+        for note, colors in new_notes.items():
+
+            if note not in old_notes:
+                note=note*note_multiplier+delta_note
+                message = mido.Message('note_on', channel=1, note=note, velocity=127)
+                port.send(message)
+            
+               # message = mido.Message('sysex', data=[1, 2, 3])
+            controller_value = int(colors[0] * attrib_coef) + controller_delta
+
+            current_control=cutoff_id
+            
+            message = mido.Message('control_change', channel=1, control=current_control, value=controller_value)
+            port.send(message)
+
+            controller_value = int(colors[1] * attrib_coef) + controller_delta
+            current_control=lfo_id
+            controller_value=int(controller_value/lfo_coef)
+            message = mido.Message('control_change', channel=1, control=lfo_id, value=controller_value)
+            port.send(message)
+
+            controller_value = int(colors[2] * attrib_coef) + controller_delta
+            current_control=lfo_speed_id
+            controller_value=int(controller_value)
+            message = mido.Message('control_change', channel=1, control=lfo_id, value=controller_value)
+            port.send(message)
+
+            
+            
+        time.sleep(0.150)
+        for note, colors in new_notes.items():
+            note=note*note_multiplier+delta_note
+            message = mido.Message('note_off', channel=1, note=note, velocity=127)
+            port.send(message)
         old_notes = new_notes
 
 while True:
@@ -43,6 +91,7 @@ while True:
                 image[0][number][1],
                 image[0][number][2]
             )
+    
     process_notes(notes_detected)
 
     # Wait until next note has arrived
